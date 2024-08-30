d108fdcebda27b889f211abc67ca16e0a867eacf
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-08-30 09:48:03 +0530

    modify login.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 hover:bg-indigo-700 hover:text-white">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
@@ -54,7 +54,7 @@
 
         <!-- Sign in with Google Button -->
         <div class="flex items-center justify-center mt-4">
-            <a href="{{ route ('google-auth') }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <a href="{{ route('google-auth') }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-100 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" style="height: 20px; margin-right: 8px;">
                 {{ __('Sign in with Google') }}
             </a>
